43dc6e513d4f4e11788e6e3a4b4adbfdf85d73c8
Ilikepizza2 <lit2021010@iiitl.ac.in>
2022-09-09 03:04:45 +0530

    first commit frontend

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "http://127.0.0.1:5000/result/" },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
